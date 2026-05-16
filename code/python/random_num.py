6981cdd424d0f727c5b65e69b1f75249bd89527a
GoldenGread <hy201007@outlook.com>
2026-05-16 18:03:33 +0800

    modified:   code/maopao_sortC++.exe 	modified:   code/out/mpout-O2.out 	modified:   code/python/random_num.py 	modified:   code/out/crout.out

--- code/python/random_num.py
+++ code/python/random_num.py
@@ -1,7 +1,12 @@
 import random
+import time
 
 def get_random_num(start:int, end:int, count:int)->list:
     ranum_list = []
     for i in range(0, count):
         ranum_list.append(random.randint(start, end))
     return ranum_list
+
+def get_time():
+    return time.time()
+
