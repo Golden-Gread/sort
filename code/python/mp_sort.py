e8f6c836b885742c27b849e0b8a83fc9801256f4
GoldenGread <hy201007@outlook.com>
2026-05-19 21:50:55 +0800

    modified:   code/python/mp_sort.py 	new file:   code/python/xz_sort.py 	deleted:    "~$\347\240\224\347\251\266.docx"

--- code/python/mp_sort.py
+++ code/python/mp_sort.py
@@ -5,19 +5,23 @@ groups_num=int(input("input:"))
 
 for num in range(1,groups_num+1):
     randlist=random_num.get_random_num(1,100,100*num)
-    f = open(".\\python_out\\mp_sor100t.out","a")
+    
+    list_len=len(randlist)
     start_time=random_num.get_time()
-
-    for i in range(len(randlist)):
-        for j in range(i+1,len(randlist)):
-            if randlist[i]>randlist[j]:
-                #temp=randlist[i]
-                #randlist[i]=randlist[j]
-                #randlist[j]=temp
-                random_num.swap(randlist,i,j)
+    b=False
+    while not b:
+        b=True
+        for i in range(list_len-1):
+            if randlist[i]>randlist[i+1]:
+                b=False
+                randlist[i],randlist[i+1]=randlist[i+1],randlist[i]
+    
+    
 
     end_time=random_num.get_time()
     
-    f.write("Group "+str(num)+" Time: "+str((end_time-start_time)*1000)+"ms."+"\n")
-    print("Group "+str(num)+" Time: "+str((end_time-start_time)*1000)+"ms.")
-    f.close()
+
+    
+    
+    print("Group "+str(num)+" Data Size:"+str(100*num)+" Time: "+str((end_time-start_time)*1000)+"ms.")
+    
---
+++ code/python/xz_sort.py
@@ -0,0 +1,27 @@
+import random_num
+
+groups_num=int(input("input:"))
+
+print("Selection Sort: A total of {} groups will be sorted.Data size:{}".format(groups_num,100*(groups_num+1)*groups_num/2))
+
+for num in range(1,groups_num+1):
+    randlist=random_num.get_random_num(1,100,100*num)
+    
+    list_len=len(randlist)
+    start_time=random_num.get_time()
+    
+    for i in range(list_len-1):
+        min_index=i
+        for j in range(i+1,list_len):
+            if randlist[j]<randlist[min_index]:
+                min_index=j
+        if min_index!=i:
+            randlist[i],randlist[min_index]=randlist[min_index],randlist[i]
+
+    end_time=random_num.get_time()
+    
+
+    
+    
+    print("Group "+str(num)+" Data Size:"+str(100*num)+" Time: "+str((end_time-start_time)*1000)+"ms.")
+    
