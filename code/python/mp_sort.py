bf6ef391a0fb3d7c94b5deca1f735efedcfaf49f
GoldenGread <hy201007@outlook.com>
2026-06-02 19:25:03 +0800

    modified:   code/python/mp_sort.py

--- code/python/mp_sort.py
+++ code/python/mp_sort.py
@@ -1,5 +1,21 @@
 import random_num
 import time
+from numba import jit
+
+
+@jit(nopython=True)
+def bubble_sort(randlist:list, list_len:int):
+    b=False
+    while not b:
+        b=True
+        for i in range(list_len-1):
+            if randlist[i]>randlist[i+1]:
+                b=False
+                randlist[i],randlist[i+1]=randlist[i+1],randlist[i]
+
+
+
+
 
 strat_group_num=int(input("input:"))
 groups_num=int(input("input:"))
@@ -12,20 +28,10 @@ for num in range(strat_group_num,groups_num+1):
     
     list_len=len(randlist)
     start_time=random_num.get_time()
-    b=False
-    while not b:
-        b=True
-        for i in range(list_len-1):
-            if randlist[i]>randlist[i+1]:
-                b=False
-                randlist[i],randlist[i+1]=randlist[i+1],randlist[i]
     
+    bubble_sort(randlist,list_len)
     
 
     end_time=random_num.get_time()
-    
-
-    
-    
     print("Group "+str(num)+" Data Size:"+str(100*num)+" Time: "+str((end_time-start_time)*1000)+"ms.")
     
